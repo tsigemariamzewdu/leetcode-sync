c91c937b0800eb5636a6a369f74f352e598e1f14
tsigemariamzewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-07-04 04:28:16 +0300

    Time: 30 ms (56.60%) | Memory: 11.6 MB (40.76%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+    
+        total = 0
+        prev_value = 0
+    
+    
+        for char in s:
+            current_value = roman_to_int[char]
+        
+       
+            if current_value > prev_value:
+                total += current_value - 2 * prev_value
+            else:
+                total += current_value
+        
+       
+            prev_value = current_value
+    
+        return total
