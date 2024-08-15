2921eb744f85ffaba9cf0b7d396baaeb9ec00170
tsigemariamzewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-08-15 03:33:29 +0300

    Time: 152 ms (25.55%) | Memory: 12.8 MB (73.71%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        if len(arr)<3:
+            return False
+        l=0
+        r= len(arr)-1
+
+        while l+1< len(arr)-1 and arr[l]< arr[l+1]:
+            l +=1
+        while r-1 > 0 and arr[r]<arr[r-1]:
+            r-=1
+        return l==r
+        
