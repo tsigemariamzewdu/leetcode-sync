13b1c3a651db40f410b029691dc0bab18b64ead2
tsigemariamzewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-08-26 05:14:19 +0300

    Update 132-pattern.py

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -5,10 +5,10 @@ class Solution(object):
         :rtype: bool
         """ # the nums we are given is [1,2,3,4] we are iterating in the reverse order so 
         s3=float('-inf') # this thingy right here is just to make the first condition false cause there is no number which less then s3 but we are gonna be updating s3
-        stack=[]
-        for i in range(len(nums)-1,-1,-1): 
+        stack=[] # here initializing the stack
+        for i in range(len(nums)-1,-1,-1):  # iterating in the reverse order
             if nums[i]<s3:
-                return True #4
+                return True # this thing is always false unless s3 is updated so it takes at least to iteration for s3 to be update which guaranteed the length of the pattern is still satisfied
             while stack and stack[-1]<nums[i]:
                 s3=stack.pop()
             stack.append(nums[i])
