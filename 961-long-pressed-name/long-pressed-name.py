b5528bebd26ddbd5a7796f1ec7f9eeacb86b090e
tsigemariamzewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-08-31 04:41:53 +0300

    Time: 17 ms (41.53%) | Memory: 11.7 MB (33.90%) - LeetSync

---
+++ 961-long-pressed-name/long-pressed-name.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        """
+        :type name: str
+        :type typed: str
+        :rtype: bool
+        """
+        # so now we are supposed to use a two pointers techinque
+
+        i, j = 0, 0
+        while j < len(typed):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+            elif j == 0 or typed[j] != typed[j - 1]:
+                return False
+            j += 1
+        return i == len(name)
+
