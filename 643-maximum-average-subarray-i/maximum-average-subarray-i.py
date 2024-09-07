8685880eb7c300a728252d271b4667f6da644cec
tsigemariamzewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-09-07 05:32:51 +0300

    Time: 878 ms (92.11%) | Memory: 21.2 MB (43.57%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -5,13 +5,11 @@ class Solution(object):
         :type k: int
         :rtype: float
         """
-        n=len(nums)
-        
-        window_sum=sum(nums[:k])
-        max_sum=window_sum
-        for i in range(n-k):
-            window_sum= window_sum-nums[i]+nums[i+k]
-            max_sum=max(max_sum,window_sum)
-        return float(max_sum)/k
+        wsum=sum(nums[:k])
+        maxsum=wsum
+        for i in range(len(nums)-k):
+            wsum=wsum-nums[i]+nums[i+k]
+            maxsum=max(maxsum,wsum)
+        return float(maxsum)/k
 
         
