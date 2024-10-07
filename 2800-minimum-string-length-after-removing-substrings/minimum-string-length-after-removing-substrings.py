b3d3116b22f9b15c6701807b801b9e375d61f7f7
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-10-07 19:30:37 +0300

    Time: 29 ms (54.44%) | Memory: 11.5 MB (97.78%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minLength(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = []
+        for char in s:
+            if stack and ((stack[-1] == "A" and char == "B") or (stack[-1] == "C" and char == "D")):
+                stack.pop()
+            else:
+                stack.append(char)
+        return len(stack)
