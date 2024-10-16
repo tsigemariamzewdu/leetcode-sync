65510b2e925512dd1fe683eaeaa6b8c03091e4f5
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-10-16 14:27:11 +0300

    Time: 25 ms (87.28%) | Memory: 12.1 MB (21.34%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -5,29 +5,28 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        # i solved this not using the right approach i have used my own way but it is less efficient so i need to work in making it efficent in terms of time complexity so the thing is i need to use stack and hashtable to do it let me try my best to do it
+        if not nums2:
+            return None
+
+        mydict={}
         result=[]
-        for num in nums1:
-            if num in nums2:
-                ind = nums2.index(num)
-                found = False  # Track if we found a greater element
-                for another in nums2[ind+1:]:
-                    if another > num:
-                        result.append(another)
-                        found = True
-                        break  # Stop after finding the first greater element
-                if not found:
-                    result.append(-1)  # Append -1 if no greater element found
+        stack=[]
+        stack.append(nums2[0])
+        for i in range(1,len(nums2)):
+            while stack and stack[-1]<nums2[i]:
+                mydict[stack[-1]]=nums2[i]
+                stack.pop()
+
+
+
+            stack.append(nums2[i])
+# make sure that the element that didnot get any greater element mapped with -1
+        for element in stack:
+            mydict[element]=-1
+
+            # finally checking all the elements in nums1 and putting it into the result array and return it
+        for i in range(len(nums1)):
+            result.append(mydict[nums1[i]])
         return result
-
-        # result=[]
-        # for num in nums1:
-        #     if num in nums2:
-        #         stack=[num]
-        #         ind=nums2.index(num)
-        #         for another in nums2[num:]:
-        #             if another > num:
-        #                 result.append(another)
-        #         result.append(-1)
-        # return result
-
         
