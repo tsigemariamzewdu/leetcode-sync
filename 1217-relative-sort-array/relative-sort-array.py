617a3552b5152cc68680a098fd2bef8edc10ba05
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-11-19 09:38:22 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (53.34%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,18 +1,23 @@
-class Solution(object):
-    def relativeSortArray(self, arr1, arr2):
-        """
-        :type arr1: List[int]
-        :type arr2: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        count = {}
+        for num in arr1:
+            count[num]=count.get(num,0)+1
+
         result=[]
-        for i in arr2:
-            while i in arr1:
-                arr1.remove(i)
-                result.append(i)
-        arr1.sort()
-        if arr1:
-            for j in arr1:
-                result.append(j)
+        for num in arr2:
+            if num in count:
+                result.extend([num]*count[num])
+                # after adding the element delete
+                del count[num]
+        # now add the rest in ascending order 
+        remaining = []
+        for num in count:
+            for _ in range(count[num]):  # Add each number count[num] times
+                remaining.append(num)
+        remaining.sort()  # Sort the list in ascending order
+
+        result.extend(remaining)
         return result
-        
+
+    
