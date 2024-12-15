46be82bca2f51a5fdbbe4b77a38de80c2a6c1bd9
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2024-12-15 21:00:21 +0300

    Time: 0 ms (100.00%) | Memory: 18.3 MB (21.17%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,12 +1,19 @@
-class Solution(object):
-    def moveZeroes(self, nums):
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-       
+        # so in this question what we are supposed to do is that to move all the zeros to the last we are not asked to return anything rather to do all the things in place
+        # it is a typical two pointer question 
         left=0
         for right in range(len(nums)):
             if nums[right]!=0:
                 nums[left],nums[right]=nums[right],nums[left]
-                left += 1
+                left +=1
+                
+
+            
+
+           
+
+        
