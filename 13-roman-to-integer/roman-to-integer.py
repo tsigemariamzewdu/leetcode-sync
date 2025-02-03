affa352f14b4cc1cb90f49d4d2429434754a7243
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-03 20:15:59 +0300

    Time: 9 ms (21.52%) | Memory: 17.7 MB (73.68%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,33 +1,25 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        roman_to_int = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        result=0
+        mydict={
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
         }
-    
-        total = 0
-        prev_value = 0
-    
-    
-        for char in s:
-            current_value = roman_to_int[char]
-        
-       
-            if current_value > prev_value:
-                total += current_value - 2 * prev_value
+        total=0
+        prev_val=0
+        for i in s:
+            current_val=mydict[i]
+            if current_val>prev_val:
+                total+= current_val-2*prev_val
             else:
-                total += current_value
-        
-       
-            prev_value = current_value
-    
+                total+=current_val
+            prev_val=current_val
         return total
+
+
+        
