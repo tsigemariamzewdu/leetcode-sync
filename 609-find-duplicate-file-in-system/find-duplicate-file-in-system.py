d96e81a85d8feed1f96baa4ade5ba6b9d0f69f48
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-04 17:23:33 +0300

    Time: 407 ms (5.20%) | Memory: 25.1 MB (57.89%) - LeetSync

--- 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
@@ -1,17 +1,24 @@
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
-        path_dict=defaultdict(list) #here i am gonna have an empty list for every key by default 
+        path_dict=defaultdict(list)
         for path in paths:
-            dirr,*files= path.split()
-            
-            for f in files:
-                fname,fcontent=f.split('(')
-                path_dict[fcontent].append(dirr +  "/" + fname)
-        result=[]
-        for value in path_dict.values():
-            if len(value)>1:
-                result.append(value)
+            dirr,*files=path.split(" ")
+
+            for file in files:
+                folder,content=file.split("(")
+
+                path_dict[content].append(dirr+"/"+folder)
+            result=[]
+            for value in path_dict.values():
+                if len(value)>1:
+                    result.append(value)
         return result
 
+
+        
+                
+        
+        
+        
 
         
