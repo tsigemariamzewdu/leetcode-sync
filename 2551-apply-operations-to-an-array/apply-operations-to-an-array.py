a77a43aa58161088a2cd119767f752c86fd872cf
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-06 16:56:40 +0300

    Time: 2 ms (40.90%) | Memory: 17.8 MB (63.36%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,26 +1,23 @@
-class Solution(object):
-    def applyOperations(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        l=0
-        j=l+1
-        while j<len(nums):
-            if nums[l]!=nums[j]:
-                l+=1
-                j+=1
-            elif nums[l]==nums[j]:
-                nums[l]=nums[l]*2
-                nums[j]=0
-                l+=1
-                j+=1
-
-        left=0
-        for right in range(len(nums)):
-            if nums[right]!=0:
-                nums[left],nums[right]=nums[right],nums[left]
-                left += 1
-            
-        return nums
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
         
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                nums[i]=nums[i]*2
+                nums[i+1]=0
+            else:
+                nums[i]=nums[i]
+            
+        l=0
+        for r in range(len(nums)):
+            if nums[r] !=0:
+                nums[l]=nums[r]
+                l+=1
+        while l<len(nums):
+            nums[l]=0
+            l+=1
+        return nums
+
+
+
+
