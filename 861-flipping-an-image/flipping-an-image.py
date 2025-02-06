07f63882f254e4344051d9c13cf65ea41e309aa7
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-06 14:32:37 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (33.62%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -1,14 +1,22 @@
-class Solution(object):
-    def flipAndInvertImage(self, image):
-        """
-        :type image: List[List[int]]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
         for img in image:
-            img.reverse()
-            for i  in range(len(img)):
-                if img[i]==0:
-                    img[i]=1
-                elif img[i] ==1:
+            l=0
+            r=len(img)-1
+            while l<=r:
+                img[l],img[r]=img[r],img[l]
+                l+=1
+                r-=1
+            for i in range(len(img)):
+                if img[i]==1:
                     img[i]=0
+                else:
+                    img[i]=1
         return image
+                
+
+            
+
+            
+        
+        
