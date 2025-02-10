5487955c8b5aab2882ec2f344c073eb4de9904f1
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-10 16:29:40 +0300

    Time: 378 ms (8.37%) | Memory: 18.1 MB (82.01%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -5,14 +5,16 @@ class Solution:
         for ii in range(len(heights)):
             heighttonames[heights[ii]]=names[ii]
         n=len(heights)
-        for i in range(n):
-            is_sorted=True
-            for j in range(1,n-i):
-                if heights[j]<heights[j-1]:
-                    heights[j],heights[j-1]=heights[j-1],heights[j]
-                    is_sorted=False
-            if is_sorted:
-                break
+        for i in range(1,n):
+            while heights[i]<heights[i-1]:
+                heights[i],heights[i-1]=heights[i-1],heights[i]
+                if i ==1 or i ==n:
+                    continue
+            
+                i-=1
+
+
+
         for k in range(n-1,-1,-1):
             result.append(heighttonames[heights[k]])
         return result
