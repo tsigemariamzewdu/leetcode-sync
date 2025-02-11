74c10fb1db4c32233abbdb10b4d676ee966bc8b7
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-11 06:58:08 +0300

    Time: 12 ms (17.40%) | Memory: 21.7 MB (13.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,17 +1,15 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        intervals.sort(key=lambda i : i[0])
-        output=[intervals[0]] # i dont want it to be empty so that is the first list
-        for s, e in intervals[1:]:
-            lastend=output[-1][1] #this is the most recently added list last element we want to compare this element with the start element
-            if s <= lastend:
-                output[-1][1]=max(lastend,e)
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        output=[intervals[0]]
+        for s,e in intervals[1:]:
+            last=output[-1][1]
+            if s<=last:
+                output[-1][1]=max(last,e)
             else:
                 output.append([s,e])
         return output
+
+        
 
         
