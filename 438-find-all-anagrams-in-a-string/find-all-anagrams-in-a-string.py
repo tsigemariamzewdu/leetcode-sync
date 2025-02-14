cb90ef569528354c3aaa9e364a77577bb23e787c
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-14 18:21:13 +0300

    Time: 193 ms (36.70%) | Memory: 18.7 MB (5.71%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,34 +1,40 @@
-class Solution(object):
-    def findAnagrams(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        
-        if len(p)> len(s):
-            return []  # because if this is the case we need to return empty list
-            #so this is one of the edge cases 
-        # since we are finding all the anagrams in a string we need to count the occurence of the letters so i am gonna use a dictionary for this purpose
-        pcount={}
-        scount={}
-        for i in range(len(p)):
-            pcount[p[i]]=1+pcount.get(p[i],0)
-            scount[s[i]]=1+scount.get(s[i],0)
-            # this is another way of counting i have seen it somewhere before but never used before
-        newlist=[0] if scount==pcount else []
-        # now lets assign the pointers 
-        l=0
-        for r in range(len(p),len(s)):
-            scount[s[r]]=1+scount.get(s[r],0)
-            scount[s[l]]-=1
-            if scount[s[l]]==0:
-                scount.pop(s[l])
-            l += 1
-            if scount ==pcount:
-                newlist.append(l)
-        return newlist
+from collections import Counter
 
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        target=Counter(p)
+
+        k=len(p)
+        l=0
+        res=[]
+        if k>len(s):
+            return res
+        mydict=Counter(s[:k])
+        if mydict==target:
+            res.append(l)
         
+        
+        for r in range(k,len(s)):
+            mydict[s[l]]-=1
+            
+            mydict[s[r]]+=1
+            l+=1
+            if mydict==target:
+                res.append(l)
+            if mydict[s[l]]==0:
+                del mydict[s[l]]
+           
+        return res
+            
+
+            
+        
+
+
+
+
+
+
+
 
         
