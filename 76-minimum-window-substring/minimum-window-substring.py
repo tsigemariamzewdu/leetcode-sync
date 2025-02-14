224027dca554cfb1805306c52abc628d1ea26586
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-14 11:16:47 +0300

    Time: 68 ms (69.94%) | Memory: 18.4 MB (33.90%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,49 @@
+from collections import Counter
+from collections import defaultdict
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        dict_t=Counter(t)
+        required=len(dict_t)
+        left=0
+        n=len(s)
+        acquired=0
+
+        
+        window_freq=defaultdict(int)
+        maxlen=n+1
+        rleft=0
+        rright=0
+
+        for right in range(n):
+            cur=s[right]
+            window_freq[cur]+=1
+
+            if cur in dict_t:
+                if window_freq[cur]==dict_t[cur]:
+                    acquired+=1
+            while acquired==required:
+                left_cur=s[left]
+                
+                if  right -left+1 < maxlen:
+                    maxlen=right-left+1
+                    rleft=left
+                    rright=right
+
+                    
+                window_freq[left_cur] -= 1
+                if left_cur in t:
+                    if window_freq[left_cur]<dict_t[left_cur]:
+                        acquired -= 1
+
+                left += 1
+            
+
+
+
+        if maxlen==n+1:
+            return ""
+        else:
+            return s[rleft:rright+1]
+
+
+        
