18c5c260d0c0dc0d5caa645a43bf1ad5f6fab92a
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-02-20 06:03:29 +0300

    Time: 41 ms (35.49%) | Memory: 39.5 MB (20.34%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,38 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-    #    may be i can find the middle node and reverse the second half and then check if it is the same as the first half 
-        # find the middle node
-        slow=head
-        fast=head
-        while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        cur=head
+        res=[]
         
-        # reverse the second half
-        prev=None
-        curr=slow
-        while curr:
-            nextnode=curr.next
-            curr.next=prev
-            prev=curr
-            curr=nextnode
 
-        # the last step is to check if the first half and the second half are the same 
-        left, right = head, prev
-        while right:  # Only need to check until the end of the reversed half
-            if left.val != right.val:
+        while cur:
+            res.append(cur.val)
+            cur=cur.next
+        
+        l=0
+        r=len(res)-1
+
+        while l<r:
+            if res[l]!=res[r]:
                 return False
-            left = left.next
-            right = right.next
-
+            l+=1
+            r-=1
         return True
+
         
