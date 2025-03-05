be28f8ff61c17c1a136699810e6803e6a41b7e44
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-03-05 17:38:41 +0300

    Time: 3 ms (21.72%) | Memory: 17.8 MB (80.77%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack=[]
+        opening=set(["(","[","{"])
+      
         for i in s:
-            if i in '({[':
+            if i in opening:
                 stack.append(i)
-            elif  stack and i=="]" and stack[-1]=="[":
+          
+            elif stack and i==")" and stack[-1]=="(":
                 stack.pop()
-            elif stack and  i=="}" and stack[-1]=="{":
-                stack.pop()
-            elif stack and  i==")" and stack[-1]=="(":
+            elif stack and i=="}" and stack[-1]=="{":
+                stack .pop()
+            elif stack and i=="]" and stack[-1]=="[":
                 stack.pop()
             else:
                 stack.append(i)
