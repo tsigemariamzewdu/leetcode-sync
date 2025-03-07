ed552eeda9aac28112dc08dd3f2b4e42bc3e7e85
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-03-07 18:18:40 +0300

    Time: 3 ms (64.52%) | Memory: 19 MB (97.55%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,27 +1,25 @@
-class Solution(object):
-    def evalRPN(self, tokens):
-        """
-        :type tokens: List[str]
-        :rtype: int
-        """
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
         stack=[]
         for i in tokens:
             if i in "+-*/":
-                num2=stack.pop()
-                num1=stack.pop()
-                if i =="+":
-                    stack.append(num1+num2)
-                if i =="-":
-                    stack.append(num1-num2)
-                if i =="*":
-                    stack.append(num1*num2)
-                if i =="/":
-                    if num1*num2 <0 and num1%num2 !=0:
-                        stack.append((num1//num2) +1)
+                nums2=stack.pop()
+                nums1=stack.pop()
+
+                if i=="+":
+                    stack.append(nums1+nums2)
+                if i=="-":
+                    stack.append(nums1-nums2)
+                if i=="*":
+                    stack.append(nums1*nums2)
+                if i=="/":
+                    if nums1*nums2<0 and nums1%nums2!=0:
+                        stack.append((nums1//nums2)+1)
                     else:
-                        stack.append(num1//num2)
-                    
+                        stack.append(nums1//nums2)
             else:
                 stack.append(int(i))
         return stack[0]
+            
+
         
