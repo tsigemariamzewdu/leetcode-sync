e6d23b44a098f8c3705238e52b72c8ebcc9968ee
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-03-07 18:09:59 +0300

    Time: 3 ms (43.32%) | Memory: 17.8 MB (98.18%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,32 +1,19 @@
-class Solution(object):
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        # i solved this not using the right approach i have used my own way but it is less efficient so i need to work in making it efficent in terms of time complexity so the thing is i need to use stack and hashtable to do it let me try my best to do it
-        if not nums2:
-            return None
-
-        mydict={}
-        result=[]
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack=[]
-        stack.append(nums2[0])
-        for i in range(1,len(nums2)):
-            while stack and stack[-1]<nums2[i]:
-                mydict[stack[-1]]=nums2[i]
+        ans=[]
+        value_map=defaultdict(int)
+
+        for i in range(len(nums2)):
+
+            while stack and  nums2[i]>stack[-1]:
+                value_map[stack[-1]]=nums2[i]
                 stack.pop()
-
-
-
             stack.append(nums2[i])
-# make sure that the element that didnot get any greater element mapped with -1
-        for element in stack:
-            mydict[element]=-1
+        for num in nums1:
+            ans.append(value_map[num]) if value_map[num]!=0 else ans.append(-1)
+        return ans
+        
 
-            # finally checking all the elements in nums1 and putting it into the result array and return it
-        for i in range(len(nums1)):
-            result.append(mydict[nums1[i]])
-        return result
+     
         
