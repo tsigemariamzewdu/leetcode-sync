eb07f0208b71d2a2317a77c87e7221ac860d9138
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-03-09 14:22:00 +0300

    Time: 107 ms (33.76%) | Memory: 35.2 MB (71.94%) - LeetSync

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -1,15 +1,14 @@
-class Solution(object):
-    def find132pattern(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """ # the nums we are given is [1,2,3,4] we are iterating in the reverse order so 
-        s3=float('-inf') # this thingy right here is just to make the first condition false cause there is no number which less then s3 but we are gonna be updating s3
-        stack=[] # here initializing the stack
-        for i in range(len(nums)-1,-1,-1):  # iterating in the reverse order
-            if nums[i]<s3:
-                return True # this thing is always false unless s3 is updated so it takes at least to iteration for s3 to be update which guaranteed the length of the pattern is still satisfied
-            while stack and stack[-1]<nums[i]:
-                s3=stack.pop()
-            stack.append(nums[i])
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack=[]
+        minn=nums[0]
+        for i in nums[1:]:
+            while stack and i>=stack[-1][0]:
+                stack.pop()
+            if stack and i > stack[-1][1]:
+                return True
+            stack.append([i,minn])
+            minn=min(minn,i)
         return False
+       
+            
