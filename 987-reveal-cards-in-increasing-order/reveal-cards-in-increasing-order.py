ea181435587457cd8fe66f776977553d3135588e
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-03-09 15:28:13 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (10.86%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -1,15 +1,18 @@
 from collections import deque
-class Solution(object):
-    def deckRevealedIncreasing(self, deck):
-        """
-        :type deck: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         deck.sort()
-        # 2,3,5,7,11,13,17
-        result=deque()
-        for i in reversed(deck): # 17,,13,11,7,5,3,2
-            if result:
-                result.appendleft(result.pop())
-            result.appendleft(i) # this right here appends 17 to the left side 
-        return list(result)
+        res=deque()
+
+        for i in reversed(deck):
+            if res:
+                res.appendleft(res.pop())
+            res.appendleft(i)
+        return list(res)
+
+       
+        
+       
+        
+            
+        
