57b4b0373d196244804b1be32d515ccd453b611d
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-03-10 17:13:13 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.48%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -12,21 +12,20 @@ class MyStack:
     def pop(self) -> int:
         while len(self.queue1) > 1:
             self.queue2.append(self.queue1.popleft())
-        # Get the last element (top of the stack)
+      
         topp = self.queue1.popleft()
-        # Swap the queues
+        
         self.queue1, self.queue2 = self.queue2, self.queue1
         return topp
 
     def top(self) -> int:
-        # Use pop to retrieve the top element, but push it back to queue1
+        
         while len(self.queue1) > 1:
             self.queue2.append(self.queue1.popleft())
         topp = self.queue1.popleft()
-        self.queue2.append(topp)  # Put the top element back in queue2
-        self.queue1, self.queue2 = self.queue2, self.queue1  # Swap again
+        self.queue2.append(topp)  
+        self.queue1, self.queue2 = self.queue2, self.queue1  
         return topp
 
     def empty(self) -> bool:
         return len(self.queue1) == 0
-
