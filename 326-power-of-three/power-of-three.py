bc929932630c7a776cfcfa6c0be2c5ec005a1691
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-03-14 10:41:40 +0300

    Time: 12 ms (45.15%) | Memory: 17.8 MB (46.06%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,11 +1,11 @@
-class Solution(object):
-    def isPowerOfThree(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        if n == 1:
-            return True
-        if n <= 0 or n % 3 != 0:
-            return False
-        return self.isPowerOfThree(n // 3)
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        def helper(n):
+            if n==1:
+                return True
+            if n%3!=0 or n==0:
+                return False
+            else:
+                return helper(n//3)
+        return helper(n)
+            
