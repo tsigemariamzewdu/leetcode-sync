6e8c77362523fa95d10db79a7443b1aa5a778bf4
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-03-28 18:51:21 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (19.83%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.py
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -1,16 +1,18 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        def is_nice_substring(substring:str)->bool:
-            char_set=set(substring)
-            for char in substring:
-                if char.swapcase() not in char_set:
-                    return False
-            return True
-        longest_nice=""
-        for start_index in range(len(s)):
-            for end_index in range(start_index+1,len(s)):
-                current_substring=s[start_index:end_index+1]
-                if is_nice_substring(current_substring):
-                    if len(current_substring)>len(longest_nice):
-                        longest_nice=current_substring
-        return longest_nice
+        _set=set(s)
+
+        for i in range(len(s)):
+            if len(s)<=1:
+                return ""
+            ch=s[i]
+            if ch.swapcase()  not in _set:
+                left=self.longestNiceSubstring(s[:i])
+                right=self.longestNiceSubstring(s[i+1:])
+
+                if len(right)>len(left):
+                    return right
+                return left
+        return s
+
+        
