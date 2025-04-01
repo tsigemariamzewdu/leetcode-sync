1059f1bc979c33e8ee6350e7f8a21bcda4d3b6c9
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-04-01 18:00:24 +0300

    Time: 683 ms (40.87%) | Memory: 25.5 MB (40.28%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,16 +1,36 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        sorted_arr=[]
-        counter=Counter(nums)
-        r=max(nums)-min(nums)+1
+        def merge(left_half,right_half) -> List[int]:
+            
+            left, right = 0, 0
+            merged = []
+            while left < len(left_half) and right < len(right_half):
+                if left_half[left] <= right_half[right]:
+                    merged.append(left_half[left])
+                    left += 1
+                else:
+                    merged.append(right_half[right])
+                    right += 1
+            
+            while left < len(left_half):
+                merged.append(left_half[left])
+                left += 1
+                
+            while right < len(right_half):
+                merged.append(right_half[right])
+                right += 1
+            
+            return merged
 
-        offset=min(nums)
-        result=[0]*r
-        for i in counter:
-            result[i-offset]=counter[i]
-        for i in range(len(result)):
-            for _ in range(result[i]):
-                sorted_arr.append(i+offset)
-        return sorted_arr
+        def mergeSort(arr):
+            mid=len(arr)//2
+            if len(arr) == 1:
+                return arr
+            rightarr=mergeSort(arr[mid:])
+            leftarr=mergeSort(arr[:mid])
 
-        
+
+            return merge(leftarr, rightarr)
+        return mergeSort(nums)
+         
+
