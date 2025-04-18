f8df04ff9960fde89fdaea37ab941fe0e45aab5f
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-04-18 22:10:17 +0300

    Time: 180 ms (40.18%) | Memory: 20.5 MB (57.04%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        
+
+        def inbound(i,j):
+            return 0<=i<len(mat) and 0<=j<len(mat[0])
+        directions=[(0,1),(1,0),(0,-1),(-1,0)]
+
+        rows=len(mat)
+        cols=len(mat[0])
+
+        q=deque()
+
+
+
+        for i in range(rows):
+            for j in range(cols):
+
+                if mat[i][j]==0:
+                    q.append((i,j))
+        result=[[0]*cols for _ in range(rows)]
+
+        while q:
+            r,c=q.popleft()
+
+            for dr,dc in directions:
+                nr=r+dr
+                nc=c+dc
+
+                if inbound(nr,nc) and mat[nr][nc]!=0:
+                    result[nr][nc]=result[r][c]+1
+                    mat[nr][nc]=0
+                    q.append((nr,nc))
+        return result
