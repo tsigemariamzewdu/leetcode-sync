3b6fa3eda896cd1d6b609648f9b88c09294139de
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-04-20 23:57:20 +0300

    Time: 44 ms (73.61%) | Memory: 19.2 MB (34.89%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+    
+        queue = deque([root])
+        depth = 0
+        
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                for child in node.children:
+                    queue.append(child)
+        
+        return depth
+            
