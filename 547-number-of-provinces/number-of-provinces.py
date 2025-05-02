0754835d4a02dbb552f35b80f477904b2cce7c11
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-05-02 18:26:54 +0300

    Time: 8 ms (38.26%) | Memory: 19.1 MB (56.17%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,28 +1,38 @@
+class UnionFind:
+    def __init__ (self,size):
+        self.root=[i for i in range(size)]
+        self.rank=[0]* size
+    def find(self,x):
+        while x!= self.root[x]:
+            self.root[x]=self.root[self.root[x]]
+            x=self.root[x]
+        return self.root[x]
+    def union(self,x,y):
+        rootx,rooty=self.find(x),self.find(y)
+
+        if rootx!=rooty:
+            rankx=self.rank[rootx]
+            ranky=self.rank[rooty]
+
+
+            if rankx>ranky:
+                self.root[rooty]=rootx
+            elif rankx<ranky:
+                self.root[rootx]=rooty
+            else:
+                self.root[rooty]=rootx
+                self.rank[rootx]+=1
+
+
 class Solution:
     def findCircleNum(self, grid: List[List[int]]) -> int:
-        visited = [False]*len(grid)
      
-        
+        n=len(grid)
+        dsu=UnionFind(n)
 
-        def dfs(node):
-            visited[node]=True
-            for  nei in range(len(grid)):
-                if grid[node][nei]==1 and not visited[nei]:
-                    dfs(nei)
-            
-
-        province=0
-        for i in range(len(grid)):
-            
-                if not visited[i]:
-                    dfs(i)
-                    province+=1
-                    
-
-       
-        return province
-                
-
-
-
-
+        for citya in range(n):
+            for cityb in range(citya+1,n):
+                if grid[citya][cityb] and dsu.find(citya)!=dsu.find(cityb):
+                    dsu.union(citya,cityb)
+        components=[dsu.find(i) for i in range(n)]
+        return len(set(components))
