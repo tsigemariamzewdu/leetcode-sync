0dbeb771563b9901aa384154c0f300daf7a82731
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-05-14 09:00:00 +0300

    Time: 5 ms (42.22%) | Memory: 18.9 MB (34.17%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,8 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        maxi=max(nums)
-        setn=set(nums)
-        for i in range(len(nums)+1):
-            if i not in setn:
-                return i
+        n=len(nums)
+
+        for i,num in enumerate(nums):
+            n = n^i^num
+        return n
+  
+     
         
