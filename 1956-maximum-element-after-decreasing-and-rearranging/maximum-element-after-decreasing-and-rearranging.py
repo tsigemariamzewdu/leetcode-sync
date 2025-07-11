c6c1d679b021042fb6e8386bd09f54ec9bd634ca
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-07-11 20:26:07 +0300

    Time: 35 ms (46.11%) | Memory: 27.5 MB (68.91%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.py
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.py
@@ -6,7 +6,7 @@ class Solution:
 
         for i in range(1,len(arr)):
             if abs(arr[i]-arr[i-1])>1:
-                arr[i]=arr[i-1]+1
+                arr[i]=1+arr[i-1]
        
         return max(arr)
 
