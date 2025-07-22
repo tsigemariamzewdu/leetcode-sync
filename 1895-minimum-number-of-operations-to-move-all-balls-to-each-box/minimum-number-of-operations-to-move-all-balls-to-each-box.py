92ada1813e50be1133f14d9d8d53b6ff4d98ae91
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-07-22 22:31:00 +0300

    Time: 3538 ms (9.15%) | Memory: 17.9 MB (93.26%) - LeetSync

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        ans=[0]*len(boxes)
+        for i in range(len(boxes)):
+            for j in range(len(boxes)):
+                if i!=j and  boxes[j]=="1":
+                    ans[i]+= abs(i-j)
+                   
+        return ans
+
+        
