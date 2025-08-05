9838a87298213884542187376756144061f776f5
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-08-05 06:47:04 +0300

    Time: 0 ms (100.00%) | Memory: 18.6 MB (73.60%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,17 +1,16 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left=0
-        right=len(nums)-1
-        
-        while left<=right:
-            mid=(left+right)//2
-            
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+           
+
             if nums[mid]==target:
                 return mid
-            elif nums[mid]<target:
-                left=mid+1
-
+            elif nums[mid]>target:
+                high=mid-1
             else:
-                right=mid-1
+                low=mid+1
         return -1
         
