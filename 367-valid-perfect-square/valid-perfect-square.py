aa3764a501a22d456fb15860c25caf9e328ef02b
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-08-07 21:57:06 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (31.91%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -6,7 +6,16 @@ class Solution(object):
         """
         if num==1:
             return True
-        for i in range(math.isqrt(num) + 1):
-            if i*i ==num:
+        high=math.isqrt(num)+1
+        low=1
+
+        while low<=high:
+            mid=(low+high)//2
+            if mid*mid==num:
                 return True
+            elif mid*mid<num:
+                low=mid+1
+            else:
+                high=mid-1
         return False
+       
