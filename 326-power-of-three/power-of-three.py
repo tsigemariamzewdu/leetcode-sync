ef309d14c63da9e6e6d5181d30f8425fa433146b
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-08-13 20:55:18 +0300

    Time: 15 ms (31.52%) | Memory: 18.2 MB (5.76%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -3,9 +3,8 @@ class Solution:
         def helper(n):
             if n==1:
                 return True
-            if n%3!=0 or n==0:
+            elif n%3!=0 or n==0:
                 return False
             else:
                 return helper(n//3)
         return helper(n)
-            
