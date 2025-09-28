79cdf02d81af25e76940290ab4bb58459ecc421d
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-09-28 21:28:35 +0300

    Time: 3 ms (21.48%) | Memory: 18.2 MB (5.99%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,4 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(n-1+m-1,n-1)
+        memo={}
+
+        def dp(i,j):
+            if i>=m or j>=n:
+                return 0
+            if i==m-1 and j==n-1:
+                return 1
+            state=(i,j)
+            if state not in memo:
+                memo[state]=dp(i+1,j)+dp(i,j+1)
+            return memo[state]
+        return dp(0,0)
         
