4c7da45a9d304eb5a3f4c4c90f5d6c7cd2256c0f
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-09-29 17:52:10 +0300

    Time: 72 ms (10.52%) | Memory: 27.5 MB (6.38%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,13 +1,15 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        @cache
+        memo={}
         def dp(i,j):
             if j<0 or j==len(matrix[0]):
                 return float("inf")
             if i==len(matrix):
                 return 0
-            return matrix[i][j]+min(dp(i+1,j-1),dp(i+1,j),dp(i+1,j+1))
-
+            state=(i,j)
+            if state not in memo:
+                memo[state]=matrix[i][j]+min(dp(i+1,j-1),dp(i+1,j),dp(i+1,j+1))
+            return memo[state]
         minCost=float("inf")
         for i in range(len(matrix[0])):
             minCost=min(minCost,dp(0,i))
