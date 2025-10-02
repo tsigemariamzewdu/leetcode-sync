e6f27a7eca1e99c7f6d333cc197cf73d1698f550
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-10-02 06:08:25 +0300

    Time: 15 ms (9.04%) | Memory: 19.6 MB (16.79%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,15 +1,22 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        memo={}
-        def dp(i,j):
-            if i>=len(triangle):
-                return 0
-            if j>i:
-                return float("inf")
-            state=(i,j)
+        dp=[[0] * (r+1) for r in range(len(triangle))]
 
-            if state not in memo:
-                memo[state]=triangle[i][j]+min(dp(i+1,j),dp(i+1,j+1))
-            return memo[state]
-        return dp(0,0)
+        def inbound(r,c):
+            return 0<=r<len(triangle) and 0<=c<=r
+        
+        dp[0][0]=triangle[0][0]
+
+        for i in range(len(triangle)):
+            for j in range(i+1):
+                up=float("inf")
+                left=float("inf")
+                if i==0 and j==0:
+                    continue
+                if inbound(i-1,j):
+                    up=triangle[i][j]+dp[i-1][j]
+                if inbound(i-1,j-1):
+                    left=triangle[i][j]+dp[i-1][j-1]
+                dp[i][j]=min(up,left)
+        return min(dp[-1])
         
