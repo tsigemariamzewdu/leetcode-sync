9b88c7b8097f3ee2d69d3c641459e72f65c1a74c
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2025-10-06 18:15:46 +0300

    Time: 98 ms (28.87%) | Memory: 36.7 MB (11.34%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -4,23 +4,12 @@ class Solution:
         count=0
         for n in arr:
             dictt[n%k]+=1
-        print(dictt)
-        for n in arr:
-            if n%k==0 and dictt[n%k]>=2:
-                count+=1
-                dictt[n%k]-=2
-            elif (dictt[k-(n%k)]>0  and dictt[n%k]>0):
-                dictt[n%k]-=1
-                if dictt[k-(n%k)]>0:
-                    dictt[k-(n%k)]-=1
-                    count+=1
+        # print(dictt)
+        for n in range(1,k):
+            if n==k-n and dictt[n]&1:
+                return False
+            if dictt[n]!=dictt[k-n]:
+                return False
+        return True
 
-        print(count)
-        return count==(len(arr)//2)
-          
-
-
-
-        print(dict)
-
-        
+       
