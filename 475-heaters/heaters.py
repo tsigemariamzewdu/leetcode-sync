55cb49a857eacd4910348d1e716083a7aee8477e
Tsigemariam Zewdu <168372498+tsigemariamzewdu@users.noreply.github.com>
2026-02-11 15:45:54 +0300

    Time: 324 ms (9.66%) | Memory: 22.6 MB (32.99%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -1,26 +1,34 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
-        houses.sort()
         heaters.sort()
-        def valid(mid):
-            p1= 0
-            p2 =0
-            while p1< len(houses) and p2 < len(heaters):
-                if abs(houses[p1] - heaters[p2])<= mid:
-                    p1 += 1
+        houses.sort()
+        def is_valid(mid):
+            p1=0
+            p2=0
+
+            while p1<len(houses) and p2<len(heaters):
+                if abs(houses[p1]-heaters[p2])<=mid:
+                    p1+=1
                 else:
-                    p2 += 1
-            while p1< len(houses):
-                if abs(houses[p1] - heaters[-1])> mid:
+                    p2+=1
+            while p1<len(houses):
+                if abs(houses[p1]-heaters[-1])>mid:
                     return False
             return True
 
-        low = 0
-        high = max(max(houses),max(heaters))
-        while high >= low:
-            mid =(high + low)//2
-            if valid(mid):
-                high = mid -1
+
+
+
+        low = 0 
+        high = max(max(heaters),max(houses))
+        ans = high
+        while high >= low :
+            mid =(low+high)//2
+
+            if is_valid(mid):
+                ans = mid
+                high = mid-1
+
             else:
-                low = mid + 1
-        return low
+                low = mid+1
+        return ans      
